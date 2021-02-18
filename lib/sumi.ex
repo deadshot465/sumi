6609be23c289db1cff43ac07e2f9981e80ed9410
Chehui Chou <deadshot465@gmail.com>
2021-02-18 21:13:56 +0900

    Add presence update.

--- lib/sumi.ex
+++ lib/sumi.ex
@@ -21,19 +21,36 @@ defmodule Sumi do
     Consumer.start_link(__MODULE__)
   end
 
+  def handle_event({:READY, _map, _ws_state}) do
+    Task.start fn ->
+      Api.update_status(:online, "Soccer", 0, nil)
+    end
+  end
+
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
     case msg.content do
       "s?ping" ->
-        Api.create_message(msg.channel_id, "Pong!")
+        start_time = Time.utc_now(Calendar.ISO)
+        task = Task.async fn ->
+          Api.create_message(msg.channel_id, "🏓 Pinging...")
+        end
+        message = Task.await(task)
+        end_time = Time.utc_now(Calendar.ISO)
+        difference = Time.diff(start_time, end_time, :millisecond)
+        Task.start fn ->
+          Api.edit_message(message, content: "🏓 Pong!\nLatency is: #{difference}.")
+        end
       "s?about" ->
-        description = "Sumi in the Church of Minamoto Kou.\nSumi was inspired by the manga/visual novel/novel The Monster of Memory.\nSumi version 0.1 was made and developed by:\n**Tetsuki Syu#1250, Kirito#9286**\nRuntime environment:\nErlang/OTP 23, Elixir 1.11.3"
-        embed = %Nostrum.Struct.Embed{}
-        |> put_color(0x585987)
-        |> put_description(description)
-        |> put_thumbnail("https://cdn.discordapp.com/emojis/291709559477895169.png")
-        |> put_author("Sumi from The Monster of Memory", "", "https://cdn.discordapp.com/avatars/806706183637041192/e53034dfdfc40f778330ac55830f6da6.webp?size=1024")
-        |> put_footer("Sumi Bot: Release 0.1 | 2021-02-07")
-        Api.create_message(msg.channel_id, embed: embed)
+        Task.start fn ->
+          description = "Sumi in the Church of Minamoto Kou.\nSumi was inspired by the manga/visual novel/novel The Monster of Memory.\nSumi version 0.1 was made and developed by:\n**Tetsuki Syu#1250, Kirito#9286**\nRuntime environment:\nErlang/OTP 23, Elixir 1.11.3"
+          embed = %Nostrum.Struct.Embed{}
+          |> put_color(0x585987)
+          |> put_description(description)
+          |> put_thumbnail("https://cdn.discordapp.com/emojis/291709559477895169.png")
+          |> put_author("Sumi from The Monster of Memory", "", "https://cdn.discordapp.com/avatars/806706183637041192/e53034dfdfc40f778330ac55830f6da6.webp?size=1024")
+          |> put_footer("Sumi Bot: Release 0.1 | 2021-02-07")
+          Api.create_message(msg.channel_id, embed: embed)
+        end
       _ ->
         :ignore
     end
